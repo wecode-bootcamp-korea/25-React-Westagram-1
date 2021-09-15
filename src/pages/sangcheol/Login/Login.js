58e36fef3c75a472b5bfa7924b399751d6bc3b82
이상철 <zlrz6706@gmail.com>
2021-09-15 21:16:26 +0900

    fisrt commit add sangcheol files

--- src/pages/sangcheol/Login/Login.js
+++ src/pages/sangcheol/Login/Login.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import './Login.scss';
+import '../../../styles/common.scss';
 
 class Login extends React.Component {
   render() {
-    return null;
+    return (
+      <div className=" sangcheol-Login">
+        <section id="section">
+          <div id="inner">
+            <h1 id="logo-text">Westagram</h1>
+            <div className="input-box">
+              <input
+                type="text"
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+                className="input-box_login"
+              />
+              <input
+                type="text"
+                placeholder="비밀번호"
+                className="input-box_password"
+              />
+              <button className="btnon off-button">로그인</button>
+              <a id="footer_link" href="Null">
+                비밀번호를 잊으셨나요 ?
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
   }
 }
 

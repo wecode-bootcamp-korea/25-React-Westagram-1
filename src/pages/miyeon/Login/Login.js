779aa406c5d609a30ed6b4ecdbb97e39e0c3f289
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-09-15 19:56:59 +0900

    Add : first commit

--- src/pages/miyeon/Login/Login.js
+++ src/pages/miyeon/Login/Login.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import './Login.scss';
 
 class Login extends React.Component {
+  goToMain = () => {
+    this.props.history.push('/Main-MiYeon');
+  };
+
   render() {
-    return null;
+    return (
+      <section>
+        <article>
+          <div className="loginBox">
+            <h1 className="catstaLogo1">
+              catstagram
+              <img src="./images/miyeon/catFoot.png" class="catFoot" />
+            </h1>
+            <div>
+              <form className="loginForm">
+                <input
+                  type="text"
+                  id="idInput"
+                  placeholder="전화번호, 사용자 이름 또는 이메일"
+                />
+                <input type="password" id="pwInput" placeholder="비밀번호" />
+              </form>
+              <button className="loginGo" onClick={this.goToMain}>
+                로그인
+              </button>
+              <a onClick={this.goToMain} className="pwForgot">
+                비밀번호를 잊으셨나요?
+              </a>
+            </div>
+          </div>
+        </article>
+      </section>
+    );
   }
 }
 
-export default Login;
+export default withRouter(Login);
--- src/pages/miyeon/Main/Main.js
+++ src/pages/miyeon/Main/Main.js
@@ -1,8 +1,204 @@
-import React from 'react';
+import { Component } from 'react';
+import './Main.scss';
 
-class Main extends React.Component {
+class Main extends Component {
   render() {
-    return null;
+    return (
+      <>
+        <header>
+          <nav>
+            <div className="leftBar">
+              <span className="catstaLogo">
+                <img src="./images/miyeon/catFoot.png" class="catFoot" />
+              </span>
+              <a href="/Main-MiYeon" className="catstaText">
+                <h1 className="catstaLogo2">catstagram</h1>
+              </a>
+            </div>
+            <div className="centerBar">
+              <input type="text" placeholder="검색" className="searchBar" />
+              <span className="searchIcon">
+                <i className="fas fa-search" />
+              </span>
+            </div>
+            <div className="rightBar">
+              <img
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+                alt="explore"
+              />
+              <img
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+                alt="heart"
+              />
+              <img
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+                alt="profile"
+              />
+            </div>
+          </nav>
+        </header>
+
+        <section>
+          <article className="wrapper">
+            <div className="feedHeader">
+              <div className="headerLeft">
+                <img
+                  className="catProfilePhoto2"
+                  src="./images/miyeon/buzzi.jpg"
+                  alt="my cutie cat"
+                />
+                <div className="myName">buzzi_nyang</div>
+              </div>
+              <div className="headerRight">
+                <img src="./images/miyeon/more.png" alt="more" />
+              </div>
+            </div>
+            <div className="feedImage">
+              <img className="myCat" src="./images/miyeon/cat.jpg" alt="cat1" />
+            </div>
+            <div className="imageBottom">
+              <div className="bottomLeft">
+                <img src="./images/miyeon/heart.png" alt="heart2" />
+                <img src="./images/miyeon/comment.png" alt="comment" />
+                <img src="./images/miyeon/upload.png" alt="upload" />
+              </div>
+              <div className="bottomRight">
+                <img
+                  className="save"
+                  src="./images/miyeon/save.png"
+                  alt="content save"
+                />
+              </div>
+            </div>
+            <div className="whosLike">
+              <div className="likePhoto">
+                <img
+                  className="catProfilePhoto4"
+                  src="./images/miyeon/buzzi.jpg"
+                  alt="cat2"
+                />
+              </div>
+              <div className="likeWho">
+                <b>buzzi_nyang</b>님 <b>외 2명이</b> 좋아합니다.
+              </div>
+            </div>
+            <div className="feedContent">
+              <b>buzzi_nyang</b>&nbsp; 박버찌 기다려.. 집사가 곧 냥줍 하러
+              간다..
+            </div>
+            <ul className="commentList">
+              <li className="comment1">
+                <b>rrpec9</b>&nbsp; 아주 사나운 맹수네요.. 미니 호랭이..
+              </li>
+              <li className="comment2">
+                <b>buzzi_nyang</b>&nbsp; 우리 버찌 너무 예쁘죠! 애교냥이에요~
+              </li>
+            </ul>
+            <div className="time">50분 전</div>
+            <div className="commentInput">
+              <input
+                className="comment"
+                type="text"
+                placeholder="댓글 달기..."
+              />
+              <button id="textUpload">게시</button>
+            </div>
+          </article>
+          <div className="mainRight">
+            <div className="myProfile">
+              <div>
+                <img
+                  className="catProfilePhoto3"
+                  src="./images/miyeon/buzzi.jpg"
+                  alt="my cutie cat"
+                />
+              </div>
+              <div className="myName">buzzi_nyang</div>
+            </div>
+            <div className="sideMenu">
+              <div className="sideTop">
+                <div className="sideLeft">스토리</div>
+                <div className="sideRight">모두 보기</div>
+              </div>
+              <div className="sideBottom">
+                <div className="user1">
+                  <img src="./images/miyeon/user1.jpg" alt="my friend's dog" />
+                  <div className="userID">
+                    <a href="../Login.js" className="whatID">
+                      <b>mydog_syuli</b>
+                    </a>
+                    <p className="beforeUpload">16분 전</p>
+                  </div>
+                </div>
+                <div className="user2">
+                  <img
+                    src="./images/miyeon/user2.jpg"
+                    alt="corona 2nd complete"
+                  />
+                  <div className="userID">
+                    <a href="../Login.js" className="whatID">
+                      <b>v_ddabong_v</b>
+                    </a>
+                    <p className="beforeUpload">1시간 전</p>
+                  </div>
+                </div>
+                <div className="user3">
+                  <img src="./images/miyeon/user3.jpg" alt="3d cat car" />
+                  <div className="userID">
+                    <a href="../Login.js" className="whatID">
+                      <b>iwascar</b>
+                    </a>
+                    <p className="beforeUpload">20시간 전</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="sideMenu">
+              <div className="sideTop">
+                <div className="sideLeft">회원님을 위한 추천</div>
+                <div className="sideRight">모두 보기</div>
+              </div>
+              <div className="sideBottom">
+                <div className="user1">
+                  <img src="./images/miyeon/cat1.jpg" alt="blue cat" />
+                  <div className="userID">
+                    <a href="../Login.js" className="whatID">
+                      <b>bluecat</b>
+                    </a>
+                    <p className="beforeUpload">16분 전</p>
+                  </div>
+                </div>
+                <div className="user2">
+                  <img src="./images/miyeon/user4.jpg" alt="3d cat" />
+                  <div className="userID">
+                    <a href="../Login.js" className="whatID">
+                      <b>cat_choding</b>
+                    </a>
+                    <p className="beforeUpload">1시간 전</p>
+                  </div>
+                </div>
+                <div className="user3">
+                  <img src="./images/miyeon/cat3.jpg" alt="white cat" />
+                  <div className="userID">
+                    <a href="../Login.js" className="whatID">
+                      <b>nyangX2_punch</b>
+                    </a>
+                    <p className="beforeUpload">20시간 전</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <footer className="fooTer">
+              <p>
+                Instagram 정보 · 지원 · 홍보 센터 · API · 채용 정보 ·
+                개인정보처리방침 · 약관 · 디렉터리 · 프로필 · 해시태그 ·언어
+              </p>
+              <p>© 2021 CATSTAGRAM</p>
+            </footer>
+          </div>
+        </section>
+      </>
+    );
   }
 }
 

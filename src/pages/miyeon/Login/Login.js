717822196bec8822437deed832202eb006f70667
miyeon <miyeon@bagmiyeon-ui-MacBookPro.local>
2021-09-20 15:46:32 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/miyeon/Login/Login.js
+++ src/pages/miyeon/Login/Login.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import LoginForm from './LoginForm';
+import { withRouter } from 'react-router-dom';
 import './Login.scss';
 
 class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      color: '#b9def7',
+      idVal: '',
+      pwVal: '',
+      buttonColor: false,
     }; // id와 pw의 state값 초기화
   }
 
+  handleIdInput = e => {
+    this.setState({
+      idVal: e.target.value,
+    });
+  }; // id값 state에 저장
+
+  handlePwInput = e => {
+    this.setState({
+      pwVal: e.target.value,
+    });
+  }; // pw값 state에 저장
+
+  buttonColorChange = e => {
+    const { idVal, pwVal } = this.state;
+    idVal.includes('@') && pwVal.length >= 5
+      ? this.setState({ buttonColor: true })
+      : this.setState({ buttonColor: false });
+  };
+
   goToMain = () => {
-    this.props.history.push('/Main-MiYeon');
+    const { idVal, pwVal } = this.state;
+    idVal == 'buzzi_nyang@happy' && pwVal == '1234'
+      ? this.props.history.push('/Main-MiYeon')
+      : alert('캣스타그램 계정 주인만 로그인할 수 있습니다');
   };
 
   render() {
@@ -24,7 +48,29 @@ class Login extends React.Component {
               <img src="./images/miyeon/catFoot.png" className="catFoot" />
             </h1>
             <div>
-              <LoginForm />
+              <form onChange={this.buttonColorChange}>
+                <input
+                  type="text"
+                  id="idInput"
+                  placeholder="전화번호, 사용자 이름 또는 이메일"
+                  onChange={this.handleIdInput}
+                />
+                <input
+                  type="password"
+                  id="pwInput"
+                  placeholder="비밀번호"
+                  onChange={this.handlePwInput}
+                />
+                <button
+                  className={this.state.buttonColor ? 'loginOn' : 'loginOff'}
+                  onClick={this.goToMain}
+                >
+                  로그인
+                </button>
+                <a onClick={this.goToMain} className="pwForgot">
+                  비밀번호를 잊으셨나요?
+                </a>
+              </form>
             </div>
           </div>
         </article>
@@ -33,4 +79,4 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+export default withRouter(Login);

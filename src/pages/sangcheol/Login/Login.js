73b83c56138dc6859e261ca328065c00b4467d12
이상철 <zlrz6706@gmail.com>
2021-09-21 13:44:08 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/sangcheol/Login/Login.js
+++ src/pages/sangcheol/Login/Login.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { withRouter } from 'react-router';
 import './Login.scss';
 import '../../../styles/common.scss';
 
 class Login extends React.Component {
+  state = {
+    id: '',
+    password: '',
+  };
+
+  handleIdInput = e => {
+    this.setState({
+      [this.state.id]: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+
+  handlePasswordInput = e => {
+    this.setState({
+      [this.state.password]: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+
+  goToMain = () => {
+    this.props.history.push('/Main-SangCheol');
+  };
+
   render() {
     return (
       <div className=" sangcheol-Login">
@@ -14,13 +38,17 @@ class Login extends React.Component {
                 type="text"
                 placeholder="전화번호, 사용자 이름 또는 이메일"
                 className="input-box_login"
+                onChange={this.handleIdInput}
               />
               <input
                 type="text"
                 placeholder="비밀번호"
                 className="input-box_password"
+                onChange={this.handlePasswordInput}
               />
-              <button className="btnon off-button">로그인</button>
+              <button className="btnon off-button" onClick={this.goToMain}>
+                로그인
+              </button>
               <a id="footer_link" href="Null">
                 비밀번호를 잊으셨나요 ?
               </a>
@@ -32,4 +60,4 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+export default withRouter(Login);
